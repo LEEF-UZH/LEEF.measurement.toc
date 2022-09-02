a0f6942ae0a597bceb0863e06f8e29fca55ecc10
Rainer M. Krug <Rainer@krugs.de>
2022-09-02 16:48:51 +0200

    Squashed commit of the following:
    commit 248fc802df81d1f29168fce5236cdf932b8a4dce
    Author: Rainer M. Krug <Rainer@krugs.de>
    Date:   Fri Sep 2 16:47:59 2022 +0200
    
        Finalised
    
    commit d7b6551b6031c223dc9e1d6ab0decd847eeeb1a1
    Author: Rainer M. Krug <Rainer@krugs.de>
    Date:   Fri Sep 2 16:15:02 2022 +0200
    
        Finalised pre_processing
    
    commit 8bd2ea52c777afb644a71921b5194b408fb2a009
    Author: Rainer M. Krug <Rainer@krugs.de>
    Date:   Fri Sep 2 10:47:48 2022 +0200
    
        tmp 2
    
    commit ecd08b6339f2dbaeebe1ee89e5827a738ee8aef2
    Author: Rainer M Krug <Rainer@krugs.de>
    Date:   Thu Sep 1 15:03:29 2022 +0200
    
        Temp 1

--- R/pre_processor_toc.R
+++ R/pre_processor_toc.R
@@ -1,147 +1,54 @@
-#' Preprocessor o2meter toc data
+#' Preprocessor toc data
 #'
-#' Split files in parts and save as csv
-#'
+#' Copy original text files
 #' @param input directory from which to read the data
 #' @param output directory to which to write the data
 #'
 #' @return invisibly \code{TRUE} when completed successful
 #'
 #' @importFrom utils read.csv write.csv
+#' @importFrom tools file_path_sans_ext
+#' @importFrom loggit set_logfile
 #'
 #' @export
 
 pre_processor_toc <- function(
-  input,
-  output
+    input,
+    output
 ) {
-  message("\n########################################################\n")
-  message("\nProcessing toc\n")
-  ##
-  toc_path <- file.path( input, "toc" )
-  toc_files <- list.files(
-    path = toc_path,
-    pattern = "*.txt",
-    full.names = TRUE,
-    recursive = TRUE
-  )
-  ##
-  tmpdir <- file.path(output, "tmp")
-  dir.create(tmpdir)
-  ##
-  lapply(
-    toc_files,
-    function(fn){
-      breaks <- which(readLines(fn) == "")
-      ##
-      header <- utils::read.csv(
-        fn,
-        skip = 0,
-        nrows = breaks[[1]],
-        header = FALSE,
-        row.names = 1,
-        col.names = c("name", "value"),
-        stringsAsFactors = FALSE
-      )
-      rownames(header) <- gsub(" |/|\\.", "_", rownames(header))
-      rownames(header) <- tolower(rownames(header))
-      #
-      layout <- utils::read.csv(
-        fn,
-        skip = breaks[[1]],
-        nrows = breaks[[2]] - breaks[[1]] - 1,
-        header = TRUE,
-        stringsAsFactors = FALSE
-      )
-      colnames(layout) <- gsub(" |/|\\.\\.|\\.", "_", colnames(layout))
-      colnames(layout) <- tolower(colnames(layout))
-      #
-      sampletime <- utils::read.csv(
-        fn,
-        skip = breaks[[2]],
-        nrows = breaks[[3]] - breaks[[2]] - 1,
-        header = FALSE,
-        row.names = 1,
-        col.names = c("name", "value"),
-        stringsAsFactors = FALSE
-      )
-      rownames(sampletime) <- tolower(rownames(sampletime))
-      #
-      colNames <- c(
-        "curve_no",
-        "type",
-        "filename",
-        "date_time",
-        "max_area",
-        "volume",
-        "max conc.",
-        "correlation",
-        "r_Squared",
-        "sel",
-        "range",
-        "empty"
-      )
-      data1 <- utils::read.csv(
-        fn,
-        skip = breaks[[3]],
-        nrows = breaks[[4]] - breaks[[3]] - 1,
-        header = FALSE,
-        fill = TRUE,
-        stringsAsFactors = FALSE,
-        col.names = colNames
-      )
-      data1 <- data1[-1,-ncol(data1)]
-      ##
-      data2 <- utils::read.csv(
-        fn,
-        skip = breaks[[4]],
-        header = FALSE,
-        fill = TRUE,
-        stringsAsFactors = FALSE
-      )
-      data2 <- data2[,-ncol(data2)]
-      names(data2) <- data2[1,]
-      data2 <- data2[-1,]
-      colnames(data2) <- gsub(" |/|\\.\\.|\\.|\\. ", "_", colnames(data2))
-      colnames(data2) <- tolower(colnames(data2))
-      ##
-      target <- file.path( tmpdir, gsub("\\.txt$", "", basename(fn)) )
-      dir.create(
-        target,
-        recursive = TRUE,
-        showWarnings = FALSE
-      )
-      ##
-      utils::write.csv( header,     file.path(target, "header.csv"),     row.names = FALSE)
-      utils::write.csv( layout,     file.path(target, "layout.csv"),     row.names = FALSE)
-      utils::write.csv( sampletime, file.path(target, "sampletime.csv"), row.names = FALSE)
-      utils::write.csv( data1,      file.path(target, "data1.csv"),      row.names = FALSE)
-      utils::write.csv( data1,      file.path(target, "data2.csv"),      row.names = FALSE)
-    }
-  )
-  ##
+  if ( length( list.files( file.path(input, "toc") ) ) == 0 ) {
+    message("\nEmpty or missing toc directory - nothing to do.\n")
+    message("\ndone\n")
+    message("########################################################\n")
+    return(invisible(TRUE))
+  }
+
   dir.create(
-    file.path( output, "toc"),
+    file.path(output, "toc"),
     recursive = TRUE,
     showWarnings = FALSE
   )
+  loggit::set_logfile(file.path(output, "toc", "toc.log"))
+
+  message("\n########################################################\n")
+  message("\nProcessing toc\n")
+  ##
+
+
+
   file.copy(
-  	file.path( input, "..", "00.general.parameter", "." ),
-  	file.path( output, "toc" ),
-  	recursive = TRUE,
-  	overwrite = TRUE
+    file.path( input, "..", "00.general.parameter", "." ),
+    file.path( output, "toc" ),
+    recursive = TRUE,
+    overwrite = TRUE
   )
+
   file.copy(
-    file.path( tmpdir, "." ),
-    to = file.path( output, "toc" ),
+    from = file.path(input, "toc", "."),
+    to = file.path(output, "toc"),
     recursive = TRUE
   )
-  ##
-  unlink(tmpdir)
-  file.copy(
-    from = file.path(input, "sample_metadata.yml"),
-    to = file.path(output, "toc", "sample_metadata.yml")
-  )
+
 
   ##
   message("done\n")
